a3278bde22e104f74335b6f8ccdb8e16617c5dbd
K.Bailey <katebailey1230@gmail.com>
2017-02-17 09:29:48 -0500

    checkpoint-30

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,12 +23,18 @@
 # Create Posts
  50.times do
  # #1
-   Post.create!(
+   post = Post.create!(
        user:   users.sample,
     topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+      
+ # #12
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+     
  end
  posts = Post.all
  
@@ -63,4 +69,5 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
  
